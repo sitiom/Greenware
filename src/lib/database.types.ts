38a7bce1368b3046db1be94932432e7a13f168aa
Ryan Caezar Itang <sitiom@proton.me>
2023-06-18 21:31:30 +0800

    chore(supabase): add migration script
    This change also moves the generated types file inside `src`

---
+++ src/lib/database.types.ts
@@ -0,0 +1,580 @@
+export type Json =
+  | string
+  | number
+  | boolean
+  | null
+  | { [key: string]: Json }
+  | Json[]
+
+export interface Database {
+  graphql_public: {
+    Tables: {
+      [_ in never]: never
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      graphql: {
+        Args: {
+          operationName?: string
+          query?: string
+          variables?: Json
+          extensions?: Json
+        }
+        Returns: Json
+      }
+    }
+    Enums: {
+      [_ in never]: never
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
+  }
+  public: {
+    Tables: {
+      cart_products: {
+        Row: {
+          created_at: string | null
+          id: number
+          product_id: number | null
+          profile_id: string | null
+          quantity: number | null
+          total: number | null
+        }
+        Insert: {
+          created_at?: string | null
+          id: number
+          product_id?: number | null
+          profile_id?: string | null
+          quantity?: number | null
+          total?: number | null
+        }
+        Update: {
+          created_at?: string | null
+          id?: number
+          product_id?: number | null
+          profile_id?: string | null
+          quantity?: number | null
+          total?: number | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "cart_products_product_id_fkey"
+            columns: ["product_id"]
+            referencedRelation: "products"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "cart_products_profile_id_fkey"
+            columns: ["profile_id"]
+            referencedRelation: "profiles"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+      favorites: {
+        Row: {
+          id: number
+          product_id: number | null
+          profile_id: string | null
+        }
+        Insert: {
+          id: number
+          product_id?: number | null
+          profile_id?: string | null
+        }
+        Update: {
+          id?: number
+          product_id?: number | null
+          profile_id?: string | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "favorites_product_id_fkey"
+            columns: ["product_id"]
+            referencedRelation: "products"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "favorites_profile_id_fkey"
+            columns: ["profile_id"]
+            referencedRelation: "profiles"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+      line_items: {
+        Row: {
+          cart_prod_id: number | null
+          created_at: string | null
+          id: number
+          order_id: number | null
+        }
+        Insert: {
+          cart_prod_id?: number | null
+          created_at?: string | null
+          id: number
+          order_id?: number | null
+        }
+        Update: {
+          cart_prod_id?: number | null
+          created_at?: string | null
+          id?: number
+          order_id?: number | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "line_items_cart_prod_id_fkey"
+            columns: ["cart_prod_id"]
+            referencedRelation: "cart_products"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "line_items_order_id_fkey"
+            columns: ["order_id"]
+            referencedRelation: "orders"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+      orders: {
+        Row: {
+          created_at: string | null
+          id: number
+          shipping_address_id: number | null
+          status: string | null
+        }
+        Insert: {
+          created_at?: string | null
+          id: number
+          shipping_address_id?: number | null
+          status?: string | null
+        }
+        Update: {
+          created_at?: string | null
+          id?: number
+          shipping_address_id?: number | null
+          status?: string | null
+        }
+        Relationships: []
+      }
+      products: {
+        Row: {
+          average_rating: number | null
+          created_at: string | null
+          discount: number | null
+          discount_price: number | null
+          id: number
+          name: string | null
+          price: number | null
+          url: string | null
+        }
+        Insert: {
+          average_rating?: number | null
+          created_at?: string | null
+          discount?: number | null
+          discount_price?: number | null
+          id: number
+          name?: string | null
+          price?: number | null
+          url?: string | null
+        }
+        Update: {
+          average_rating?: number | null
+          created_at?: string | null
+          discount?: number | null
+          discount_price?: number | null
+          id?: number
+          name?: string | null
+          price?: number | null
+          url?: string | null
+        }
+        Relationships: []
+      }
+      profiles: {
+        Row: {
+          avatar_url: string | null
+          first_name: string | null
+          id: string
+          last_name: string | null
+          updated_at: string | null
+          username: string | null
+        }
+        Insert: {
+          avatar_url?: string | null
+          first_name?: string | null
+          id: string
+          last_name?: string | null
+          updated_at?: string | null
+          username?: string | null
+        }
+        Update: {
+          avatar_url?: string | null
+          first_name?: string | null
+          id?: string
+          last_name?: string | null
+          updated_at?: string | null
+          username?: string | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "profiles_id_fkey"
+            columns: ["id"]
+            referencedRelation: "users"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+      reviews: {
+        Row: {
+          created_at: string | null
+          description: string | null
+          id: number
+          product_id: number | null
+          profile_id: string | null
+          rating: number | null
+          title: string | null
+        }
+        Insert: {
+          created_at?: string | null
+          description?: string | null
+          id: number
+          product_id?: number | null
+          profile_id?: string | null
+          rating?: number | null
+          title?: string | null
+        }
+        Update: {
+          created_at?: string | null
+          description?: string | null
+          id?: number
+          product_id?: number | null
+          profile_id?: string | null
+          rating?: number | null
+          title?: string | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "reviews_product_id_fkey"
+            columns: ["product_id"]
+            referencedRelation: "products"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "reviews_profile_id_fkey"
+            columns: ["profile_id"]
+            referencedRelation: "profiles"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+      salvaged_devices: {
+        Row: {
+          brand: string | null
+          created_at: string | null
+          id: number
+          is_working: boolean | null
+          memory: string | null
+          name: string | null
+          profiles_id: string | null
+          status: string | null
+          storage: string | null
+          url: string | null
+        }
+        Insert: {
+          brand?: string | null
+          created_at?: string | null
+          id: number
+          is_working?: boolean | null
+          memory?: string | null
+          name?: string | null
+          profiles_id?: string | null
+          status?: string | null
+          storage?: string | null
+          url?: string | null
+        }
+        Update: {
+          brand?: string | null
+          created_at?: string | null
+          id?: number
+          is_working?: boolean | null
+          memory?: string | null
+          name?: string | null
+          profiles_id?: string | null
+          status?: string | null
+          storage?: string | null
+          url?: string | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "salvaged_devices_profiles_id_fkey"
+            columns: ["profiles_id"]
+            referencedRelation: "profiles"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+      salvaged_parts: {
+        Row: {
+          created_at: string | null
+          id: number
+        }
+        Insert: {
+          created_at?: string | null
+          id: number
+        }
+        Update: {
+          created_at?: string | null
+          id?: number
+        }
+        Relationships: []
+      }
+      shipping_addresses: {
+        Row: {
+          address_line_one: string | null
+          address_line_two: string | null
+          barangay: string | null
+          created_at: string | null
+          full_name: string | null
+          id: number
+          postal_code: number | null
+          profile_id: string | null
+          province: string | null
+          region: string | null
+        }
+        Insert: {
+          address_line_one?: string | null
+          address_line_two?: string | null
+          barangay?: string | null
+          created_at?: string | null
+          full_name?: string | null
+          id: number
+          postal_code?: number | null
+          profile_id?: string | null
+          province?: string | null
+          region?: string | null
+        }
+        Update: {
+          address_line_one?: string | null
+          address_line_two?: string | null
+          barangay?: string | null
+          created_at?: string | null
+          full_name?: string | null
+          id?: number
+          postal_code?: number | null
+          profile_id?: string | null
+          province?: string | null
+          region?: string | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "shipping_addresses_profile_id_fkey"
+            columns: ["profile_id"]
+            referencedRelation: "profiles"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      [_ in never]: never
+    }
+    Enums: {
+      [_ in never]: never
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
+  }
+  storage: {
+    Tables: {
+      buckets: {
+        Row: {
+          allowed_mime_types: string[] | null
+          avif_autodetection: boolean | null
+          created_at: string | null
+          file_size_limit: number | null
+          id: string
+          name: string
+          owner: string | null
+          public: boolean | null
+          updated_at: string | null
+        }
+        Insert: {
+          allowed_mime_types?: string[] | null
+          avif_autodetection?: boolean | null
+          created_at?: string | null
+          file_size_limit?: number | null
+          id: string
+          name: string
+          owner?: string | null
+          public?: boolean | null
+          updated_at?: string | null
+        }
+        Update: {
+          allowed_mime_types?: string[] | null
+          avif_autodetection?: boolean | null
+          created_at?: string | null
+          file_size_limit?: number | null
+          id?: string
+          name?: string
+          owner?: string | null
+          public?: boolean | null
+          updated_at?: string | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "buckets_owner_fkey"
+            columns: ["owner"]
+            referencedRelation: "users"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+      migrations: {
+        Row: {
+          executed_at: string | null
+          hash: string
+          id: number
+          name: string
+        }
+        Insert: {
+          executed_at?: string | null
+          hash: string
+          id: number
+          name: string
+        }
+        Update: {
+          executed_at?: string | null
+          hash?: string
+          id?: number
+          name?: string
+        }
+        Relationships: []
+      }
+      objects: {
+        Row: {
+          bucket_id: string | null
+          created_at: string | null
+          id: string
+          last_accessed_at: string | null
+          metadata: Json | null
+          name: string | null
+          owner: string | null
+          path_tokens: string[] | null
+          updated_at: string | null
+          version: string | null
+        }
+        Insert: {
+          bucket_id?: string | null
+          created_at?: string | null
+          id?: string
+          last_accessed_at?: string | null
+          metadata?: Json | null
+          name?: string | null
+          owner?: string | null
+          path_tokens?: string[] | null
+          updated_at?: string | null
+          version?: string | null
+        }
+        Update: {
+          bucket_id?: string | null
+          created_at?: string | null
+          id?: string
+          last_accessed_at?: string | null
+          metadata?: Json | null
+          name?: string | null
+          owner?: string | null
+          path_tokens?: string[] | null
+          updated_at?: string | null
+          version?: string | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "objects_bucketId_fkey"
+            columns: ["bucket_id"]
+            referencedRelation: "buckets"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "objects_owner_fkey"
+            columns: ["owner"]
+            referencedRelation: "users"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      can_insert_object: {
+        Args: {
+          bucketid: string
+          name: string
+          owner: string
+          metadata: Json
+        }
+        Returns: undefined
+      }
+      extension: {
+        Args: {
+          name: string
+        }
+        Returns: string
+      }
+      filename: {
+        Args: {
+          name: string
+        }
+        Returns: string
+      }
+      foldername: {
+        Args: {
+          name: string
+        }
+        Returns: unknown
+      }
+      get_size_by_bucket: {
+        Args: Record<PropertyKey, never>
+        Returns: {
+          size: number
+          bucket_id: string
+        }[]
+      }
+      search: {
+        Args: {
+          prefix: string
+          bucketname: string
+          limits?: number
+          levels?: number
+          offsets?: number
+          search?: string
+          sortcolumn?: string
+          sortorder?: string
+        }
+        Returns: {
+          name: string
+          id: string
+          updated_at: string
+          created_at: string
+          last_accessed_at: string
+          metadata: Json
+        }[]
+      }
+    }
+    Enums: {
+      [_ in never]: never
+    }
+    CompositeTypes: {
+      [_ in never]: never
+    }
+  }
+}
+
---
+++ supabase/migrations/20230618123041_init.sql
@@ -0,0 +1,247 @@
+create table "public"."cart_products" (
+    "id" bigint not null,
+    "created_at" timestamp with time zone default now(),
+    "product_id" bigint,
+    "quantity" integer,
+    "profile_id" uuid,
+    "total" double precision
+);
+
+
+alter table "public"."cart_products" enable row level security;
+
+create table "public"."favorites" (
+    "id" bigint not null,
+    "product_id" bigint,
+    "profile_id" uuid
+);
+
+
+alter table "public"."favorites" enable row level security;
+
+create table "public"."line_items" (
+    "id" bigint not null,
+    "created_at" timestamp with time zone default now(),
+    "cart_prod_id" bigint,
+    "order_id" bigint
+);
+
+
+alter table "public"."line_items" enable row level security;
+
+create table "public"."orders" (
+    "id" bigint not null,
+    "created_at" timestamp with time zone default now(),
+    "shipping_address_id" bigint,
+    "status" text
+);
+
+
+alter table "public"."orders" enable row level security;
+
+create table "public"."products" (
+    "id" bigint not null,
+    "name" character varying,
+    "price" double precision,
+    "discount" double precision,
+    "discount_price" double precision,
+    "created_at" timestamp with time zone default now(),
+    "average_rating" double precision,
+    "url" text
+);
+
+
+alter table "public"."products" enable row level security;
+
+create table "public"."profiles" (
+    "id" uuid not null,
+    "updated_at" timestamp with time zone,
+    "username" text,
+    "first_name" text,
+    "last_name" text,
+    "avatar_url" text
+);
+
+
+alter table "public"."profiles" enable row level security;
+
+create table "public"."reviews" (
+    "id" bigint not null,
+    "title" character varying,
+    "description" character varying,
+    "rating" smallint,
+    "created_at" timestamp with time zone default now(),
+    "profile_id" uuid,
+    "product_id" bigint
+);
+
+
+alter table "public"."reviews" enable row level security;
+
+create table "public"."salvaged_devices" (
+    "id" bigint not null,
+    "created_at" timestamp with time zone default now(),
+    "name" text,
+    "brand" text,
+    "is_working" boolean,
+    "storage" text,
+    "memory" text,
+    "status" text,
+    "profiles_id" uuid,
+    "url" text
+);
+
+
+alter table "public"."salvaged_devices" enable row level security;
+
+create table "public"."salvaged_parts" (
+    "id" bigint not null,
+    "created_at" timestamp with time zone default now()
+);
+
+
+create table "public"."shipping_addresses" (
+    "id" bigint not null,
+    "created_at" timestamp with time zone default now(),
+    "address_line_one" text,
+    "address_line_two" text,
+    "full_name" text,
+    "region" text,
+    "province" text,
+    "barangay" text,
+    "postal_code" integer,
+    "profile_id" uuid
+);
+
+
+alter table "public"."shipping_addresses" enable row level security;
+
+CREATE UNIQUE INDEX cart_products_pkey ON public.cart_products USING btree (id);
+
+CREATE UNIQUE INDEX favorites_pkey ON public.favorites USING btree (id);
+
+CREATE UNIQUE INDEX line_items_pkey ON public.line_items USING btree (id);
+
+CREATE UNIQUE INDEX orders_pkey ON public.orders USING btree (id);
+
+CREATE UNIQUE INDEX products_pkey ON public.products USING btree (id);
+
+CREATE UNIQUE INDEX profiles_pkey ON public.profiles USING btree (id);
+
+CREATE UNIQUE INDEX profiles_username_key ON public.profiles USING btree (username);
+
+CREATE UNIQUE INDEX reviews_pkey ON public.reviews USING btree (id);
+
+CREATE UNIQUE INDEX salvaged_devices_pkey ON public.salvaged_devices USING btree (id);
+
+CREATE UNIQUE INDEX salvaged_parts_pkey ON public.salvaged_parts USING btree (id);
+
+CREATE UNIQUE INDEX shipping_addresses_pkey ON public.shipping_addresses USING btree (id);
+
+alter table "public"."cart_products" add constraint "cart_products_pkey" PRIMARY KEY using index "cart_products_pkey";
+
+alter table "public"."favorites" add constraint "favorites_pkey" PRIMARY KEY using index "favorites_pkey";
+
+alter table "public"."line_items" add constraint "line_items_pkey" PRIMARY KEY using index "line_items_pkey";
+
+alter table "public"."orders" add constraint "orders_pkey" PRIMARY KEY using index "orders_pkey";
+
+alter table "public"."products" add constraint "products_pkey" PRIMARY KEY using index "products_pkey";
+
+alter table "public"."profiles" add constraint "profiles_pkey" PRIMARY KEY using index "profiles_pkey";
+
+alter table "public"."reviews" add constraint "reviews_pkey" PRIMARY KEY using index "reviews_pkey";
+
+alter table "public"."salvaged_devices" add constraint "salvaged_devices_pkey" PRIMARY KEY using index "salvaged_devices_pkey";
+
+alter table "public"."salvaged_parts" add constraint "salvaged_parts_pkey" PRIMARY KEY using index "salvaged_parts_pkey";
+
+alter table "public"."shipping_addresses" add constraint "shipping_addresses_pkey" PRIMARY KEY using index "shipping_addresses_pkey";
+
+alter table "public"."cart_products" add constraint "cart_products_product_id_fkey" FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE not valid;
+
+alter table "public"."cart_products" validate constraint "cart_products_product_id_fkey";
+
+alter table "public"."cart_products" add constraint "cart_products_profile_id_fkey" FOREIGN KEY (profile_id) REFERENCES profiles(id) ON DELETE CASCADE not valid;
+
+alter table "public"."cart_products" validate constraint "cart_products_profile_id_fkey";
+
+alter table "public"."favorites" add constraint "favorites_product_id_fkey" FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE not valid;
+
+alter table "public"."favorites" validate constraint "favorites_product_id_fkey";
+
+alter table "public"."favorites" add constraint "favorites_profile_id_fkey" FOREIGN KEY (profile_id) REFERENCES profiles(id) ON DELETE CASCADE not valid;
+
+alter table "public"."favorites" validate constraint "favorites_profile_id_fkey";
+
+alter table "public"."line_items" add constraint "line_items_cart_prod_id_fkey" FOREIGN KEY (cart_prod_id) REFERENCES cart_products(id) not valid;
+
+alter table "public"."line_items" validate constraint "line_items_cart_prod_id_fkey";
+
+alter table "public"."line_items" add constraint "line_items_order_id_fkey" FOREIGN KEY (order_id) REFERENCES orders(id) ON DELETE CASCADE not valid;
+
+alter table "public"."line_items" validate constraint "line_items_order_id_fkey";
+
+alter table "public"."profiles" add constraint "profiles_id_fkey" FOREIGN KEY (id) REFERENCES auth.users(id) ON DELETE CASCADE not valid;
+
+alter table "public"."profiles" validate constraint "profiles_id_fkey";
+
+alter table "public"."profiles" add constraint "profiles_username_key" UNIQUE using index "profiles_username_key";
+
+alter table "public"."profiles" add constraint "username_length" CHECK ((char_length(username) >= 3)) not valid;
+
+alter table "public"."profiles" validate constraint "username_length";
+
+alter table "public"."reviews" add constraint "reviews_product_id_fkey" FOREIGN KEY (product_id) REFERENCES products(id) not valid;
+
+alter table "public"."reviews" validate constraint "reviews_product_id_fkey";
+
+alter table "public"."reviews" add constraint "reviews_profile_id_fkey" FOREIGN KEY (profile_id) REFERENCES profiles(id) ON DELETE CASCADE not valid;
+
+alter table "public"."reviews" validate constraint "reviews_profile_id_fkey";
+
+alter table "public"."salvaged_devices" add constraint "salvaged_devices_profiles_id_fkey" FOREIGN KEY (profiles_id) REFERENCES profiles(id) ON DELETE CASCADE not valid;
+
+alter table "public"."salvaged_devices" validate constraint "salvaged_devices_profiles_id_fkey";
+
+alter table "public"."shipping_addresses" add constraint "shipping_addresses_profile_id_fkey" FOREIGN KEY (profile_id) REFERENCES profiles(id) ON DELETE CASCADE not valid;
+
+alter table "public"."shipping_addresses" validate constraint "shipping_addresses_profile_id_fkey";
+
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.handle_new_user()
+ RETURNS trigger
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+AS $function$
+begin
+  insert into public.profiles (id, full_name, avatar_url)
+  values (new.id, new.raw_user_meta_data->>'full_name', new.raw_user_meta_data->>'avatar_url');
+  return new;
+end;
+$function$
+;
+
+create policy "Public profiles are viewable by everyone."
+on "public"."profiles"
+as permissive
+for select
+to public
+using (true);
+
+
+create policy "Users can insert their own profile."
+on "public"."profiles"
+as permissive
+for insert
+to public
+with check ((auth.uid() = id));
+
+
+create policy "Users can update own profile."
+on "public"."profiles"
+as permissive
+for update
+to public
+using ((auth.uid() = id));

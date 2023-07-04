bb6eb3ca990ebd9b00e08c1ef79ef9a8865f812e
NicoleMagallanes <nicolemagallanes026@gmail.com>
2023-07-04 21:43:35 +0800

    feat: Add Product Detail Page

---
+++ src/components/BreadCrumbs.tsx
@@ -0,0 +1,31 @@
+export default function BreadCrumbs() {
+  return (
+    <div>
+      <nav className="w-full rounded-md">
+        <ol className="list-reset flex">
+          <li>
+            <a
+              href="#"
+              className="hover:text-primary-600 focus:text-primary-600 active:text-primary-700 dark:text-primary-400 dark:hover:text-primary-500 dark:focus:text-primary-500 dark:active:text-primary-600 text-primary transition duration-150 ease-in-out"
+            >
+              Home
+            </a>
+          </li>
+          <li>
+            <span className="mx-2 text-neutral-500 dark:text-neutral-400">
+              /
+            </span>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="hover:text-primary-600 focus:text-primary-600 active:text-primary-700 dark:text-primary-400 dark:hover:text-primary-500 dark:focus:text-primary-500 dark:active:text-primary-600 text-primary transition duration-150 ease-in-out"
+            >
+              Product
+            </a>
+          </li>
+        </ol>
+      </nav>
+    </div>
+  );
+}
